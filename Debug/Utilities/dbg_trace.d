902ec16a001f3c0de4deb1d304ea34f45261aa10
JC-Toussaint <jean-christophe.toussaint@grenoble-inp.fr>
2020-12-30 16:35:48 +0100

    debug_trace2

--- Debug/Utilities/dbg_trace.d
+++
@@ -1,9 +0,0 @@
-Utilities/dbg_trace.o: ../Utilities/dbg_trace.c \
- ../Utilities/utilities_common.h ../Utilities/stm_queue.h \
- ../Utilities/dbg_trace.h
-
-../Utilities/utilities_common.h:
-
-../Utilities/stm_queue.h:
-
-../Utilities/dbg_trace.h:
--- Debug/Utilities/stm_queue.d
+++
@@ -1,6 +0,0 @@
-Utilities/stm_queue.o: ../Utilities/stm_queue.c \
- ../Utilities/utilities_common.h ../Utilities/stm_queue.h
-
-../Utilities/utilities_common.h:
-
-../Utilities/stm_queue.h:
